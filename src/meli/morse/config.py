96de4f51a6f60206f53fa59ac939be4326859334
JILP <jlopezpecora@gmail.com>
2020-02-08 19:32:15 -0300

    Heroku deploy configuration

--- src/meli/morse/config.py
+++ src/meli/morse/config.py
@@ -1,5 +1,6 @@
+import logging
+from logging import StreamHandler
 import os
-
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Th1s 5h0uld b3 h4rd 70 gu355'
@@ -22,11 +23,23 @@ class ProdConfig(Config):
     PROD = True
 
 
+class HerokuConfig(ProdConfig):
+
+    @classmethod
+    def init_app(cls, app):
+        ProdConfig.init_app(app)
+        # log to stderr
+        file_handler = StreamHandler()
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+
 def config(env):
     cfg = {
         'dev': DevConfig,
         'test': TestConfig,
         'prod': ProdConfig,
+        'heroku': HerokuConfig,
         'default': DevConfig
     }
     return cfg[env]
